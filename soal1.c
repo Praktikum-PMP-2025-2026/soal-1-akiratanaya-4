408028c04456519243c21a09442e6b18d700736a
Akira Kenji tanaya <akirakenji78@gmail.com>
2026-04-21 08:52:04 +0700

    Refactor input handling and loop variables in soal1.c
    Updated input method and loop variable types for better safety.

--- soal1.c
+++ soal1.c
@@ -5,15 +5,18 @@ int main(void) {
 
     char string [100];
     char new_string[100];
-    gets("%s",string);
+    gets(string);
     // printf("Panjang : %ld\n",strlen(string));
     // printf("string = %s\n", string);
     // printf("string [o]:%c\n", string[0]);
     int n=0 ;
 
-    for (int i =0 ; i<strlen(string); i++){
+    for (long unsigned int i =0 ; i<strlen(string); i++){
         if (string[i]== '('){
-            for (int j =i ; j<strlen(string); j++){
+            for (long unsigned int j =i ; j<strlen(string); j++){
+                if (string[j]=='('){
+                    string[i]==string[j];
+                }
                 if (string[j]==')'){
                     new_string[n]= string[i];
                     string[j]='M';
